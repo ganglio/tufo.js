3dc6e889762ce3a95ccabe0563a3546e2425a47a
Roberto Torella <roberto.torella@gmail.com>
2013-01-30 14:27:45 +0000

    Added test/tmp to gitignore [ci skip]

--- test/scaffolder.test.js
+++ test/scaffolder.test.js
@@ -1,21 +1,34 @@
 var scaffolder = require('../lib/scaffolder'),
-    fs = require('fs'),
+    utile = require("utile"),
+    fs = require("fs"),
     vows = require('vows'),
-    assert = require('assert');
+    assert = require('assert'),
+    path = require("path");
 
-function clean(blogname) {
-	fs.rmdirSync(blogname);
-}
+var base_path = path.join(__dirname,'tmp');
 
 vows.describe('scaffolder').addBatch({
 	'Testing methods': {
 		topic: function() { return scaffolder; },
-		'init()': function(topic) {
-			topic.init('test_blog',function(err){
-				console.log('ga');
-			});
-			console.log('gu');
-			return false;
+		'init()': {
+			topic: function() {
+				scaffolder.init('test_blog',this.callback,base_path);
+			},
+			'Async test':function(err) {
+				assert.isTrue(err);
+
+				fs.exists('test_blog',function(err){
+					assert.isTrue(err);
+					var name = require(path.join(base_path,'test_blog','tufo.json')).blog_name;
+					assert.equal(name,'test_blog');
+					utile.rimraf(path.join(base_path,'test_blog'),function(err){
+						assert.isTrue(err);
+					});
+				});
+			}
+		},
+		'newPost()': function(topic) {
+			assert.isTrue(false);
 		}
 	}
 }).export(module);
